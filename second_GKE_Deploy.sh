3eb9f2862d9baa8843ad3557c4bf90ab29c28f7a
Sudharsan S <52055923+sudharsudhar@users.noreply.github.com>
2023-11-16 23:25:54 +0530

    Update second_GKE_Deploy.sh

--- second_GKE_Deploy.sh
+++ second_GKE_Deploy.sh
@@ -1,5 +1,19 @@
-kubectl apply -f backend-deployment.yaml
-kubectl apply -f backend-service.yaml
+# kubectl apply -f backend-deployment.yaml
+# kubectl apply -f backend-service.yaml
 
-kubectl apply -f frontend-deployment.yaml
-kubectl apply -f frontend-service.yaml
+# kubectl apply -f frontend-deployment.yaml
+# kubectl apply -f frontend-service.yaml
+
+# 1. Create the "frontend" namespace
+kubectl create namespace frontend
+
+# 2. Deploy frontend microservices to the "frontend" namespace
+kubectl apply -f frontend-deployment.yaml --namespace=frontend
+kubectl apply -f frontend-service.yaml --namespace=frontend
+
+# 3. Create the "backend" namespace
+kubectl create namespace backend
+
+# 4. Deploy backend microservices to the "backend" namespace
+kubectl apply -f backend-deployment.yaml --namespace=backend
+kubectl apply -f backend-service.yaml --namespace=backend
